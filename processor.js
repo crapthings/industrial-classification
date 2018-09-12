467721b3fee7ceb655567efd54bddc024a9805bb
crapthings <crapthings@gmail.com>
2018-09-12 09:53:06 +0800

    minor update

--- processor.js
+++ processor.js
@@ -5,9 +5,29 @@ const jsonfile = require('jsonfile')
 test()
 
 async function test() {
+  const data = []
+
   const json = await jsonfile.readFile('./行业分类.json')
   traverse(json).forEach(function (node) {
-    if (node.title)
-      console.log(node.title, _.reject(this.path, item => item == 'children'))
+    if (node.title) {
+      const _node = {}
+      _node.name = node.title
+      _node.category = this.path[0]
+      _node.code = node.key
+      if (_node.code) {
+        _node.parentCode = _node.code.substring(0, _node.code.length - 1)
+        const c1 = _node.code.substring(0, 2)
+        const c2 = _node.code.substring(0, 3)
+        const c3 = _node.code.substring(0, 4)
+        _node.path = _node.category + '.' + c1
+        if (c2.length == 3)
+          _node.path += '.' + c2
+        if (c3.length == 4)
+          _node.path += '.' + c3
+      }
+      data.push(_node)
+    }
   })
+  await jsonfile.writeFile('./行业分类flat.json', data, { spaces: 2 })
+  console.log(JSON.stringify(data, null, 2))
 }
